e83d671ff983e0cfd3f1fc674baa22194624f0dd
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-06-12 19:38:04 +0600

    Api Mutation Added

---
+++ src/lib/core/server.js
@@ -0,0 +1,16 @@
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const serverMutation = async (path, data) => {
+    const res = await fetch(`${baseUrl}${path}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+
+    // Handle errors if needed 401 or 403 for example 404 or 500
+
+    return res.json();
+}
